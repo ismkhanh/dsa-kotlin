550a49275da72ea8788688d55f8169da4f6d27ab
Ismail Khan <ismail.khan@careem.com>
2026-05-31 20:52:35 +0400

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.kt
@@ -0,0 +1,25 @@
+class Solution {
+    fun topKFrequent(nums: IntArray, k: Int): IntArray {
+        val map = mutableMapOf<Int, Int>()
+        for (num in nums) {
+            if (map.containsKey(num)) map[num] = map[num]!!+1
+            else map[num] = 1
+        }
+
+        val pq = PriorityQueue<Pair<Int, Int>>(compareBy {it.second})
+        for (entry in map) {
+            val (key, value) = entry
+            pq.add(Pair(key, value))
+            if(pq.size > k) {
+                pq.poll()
+            }
+        }
+        val ans = IntArray(k) {0}
+        var index = 0
+        while (pq.isNotEmpty()) {
+            ans[index] = pq.poll().first
+            index++
+        }
+        return ans
+    }
+}
