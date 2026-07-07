caf6823f0941bc3aff46754b3cb52f401baa77f8
Ismail Khan <ismkhanh@gmail.com>
2026-07-07 13:54:21 +0400

    Add: partition-equal-subset-sum - submission-5

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-5.kt
@@ -0,0 +1,25 @@
+class Solution {
+    fun canPartition(nums: IntArray): Boolean {
+        val total = nums.sum()
+        if (total % 2 != 0) return false
+        val target = total / 2
+        val cache = Array(nums.size) { IntArray(target + 1) {-1}}
+        return isPossible(0, total/2, nums, cache)
+    }
+
+    private fun isPossible(
+        index: Int,
+        target: Int,
+        nums: IntArray,
+        cache: Array<IntArray>
+    ): Boolean {
+        if (index >= nums.size || target < 0) return false
+        if (target == 0) return true
+        if (cache[index][target] != -1) return cache[index][target] == 1
+        val take = isPossible(index+1, target - nums[index], nums, cache)
+        val dontTake = isPossible(index+1, target, nums, cache)
+        val res = take || dontTake
+        cache[index][target] = if (res) 1 else 0
+        return res
+    }
+}
